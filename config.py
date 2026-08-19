c2a7bc06dc104c7237893fc566606cab97b02550
jdio96736-dot <jdio96736@gmail.com>
2026-08-19 10:31:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "29568441"))
-API_HASH = os.getenv("API_HASH", "b32ec0fb66d22da6f77d355fbace4f2a")
+API_ID = int(os.getenv("API_ID", "39247548"))
+API_HASH = os.getenv("API_HASH", "54496b848af8f320019f4b4d174ce935")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SESSION_STRING = os.getenv("ASSISTANT_SESSION") or os.getenv("STRING_SESSION")
-MAIN_OWNER = int(os.getenv("OWNER_ID", "8673494392"))
-DEPLOYED_OWNER_ID = int(os.getenv("OWNER_ID", "8673494392"))
+MAIN_OWNER = int(os.getenv("OWNER_ID", "6983361101"))
+DEPLOYED_OWNER_ID = int(os.getenv("OWNER_ID", "6983361101"))
 SEARCH_API_URL = os.getenv("SEARCH_API_URL", "https://search-api.kustbotsweb.workers.dev")
 DOWNLOAD_API_BASE = os.getenv("DOWNLOAD_API_BASE", "").rstrip("/")
 COOKIES_FILE = os.getenv("COOKIES_FILE", "cookies.txt")
